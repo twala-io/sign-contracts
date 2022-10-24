5df4df1736530229658e8cb3c21ec50742dc6b00
Michael Tulagan <mtulagan@polker.io>
2022-10-25 03:21:36 +0800

    Format document

--- PullPayment/PullPaymentEscrow.sol
+++ PullPayment/PullPaymentEscrow.sol
@@ -7,29 +7,34 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/PullPayment.sol";
 
 contract PullPaymentEscrow is Ownable, AccessControl, PullPayment {
-  bytes32 public constant PAYEE_ROLE = keccak256("PAYEE_ROLE");
-  bool isApprove = false;
-  address public payee;
+    bytes32 public constant PAYEE_ROLE = keccak256("PAYEE_ROLE");
+    bool isApprove = false;
+    address public payee;
 
-  constructor(address _payee) payable {
-    require(msg.value > 0, "Payment: Must provide funds");
-    payee = _payee;
-    _grantRole(PAYEE_ROLE, payee);
-  }
+    constructor(address _payee) payable {
+        require(msg.value > 0, "Payment: Must provide funds");
+        payee = _payee;
+        _grantRole(PAYEE_ROLE, payee);
+    }
 
-  function releasePayments() public onlyOwner {
-    isApprove = true;
-    super._asyncTransfer(payee, address(this).balance);
-  }
+    function releasePayments() public onlyOwner {
+        isApprove = true;
+        super._asyncTransfer(payee, address(this).balance);
+    }
 
-  function withdrawPayments(address payable _payee) public override onlyRole(PAYEE_ROLE) virtual {
-    require(_payee == payee, "Payment: Invalid payee address");
-    super.withdrawPayments(_payee);
-  }
+    function withdrawPayments(address payable _payee)
+        public
+        virtual
+        override
+        onlyRole(PAYEE_ROLE)
+    {
+        require(_payee == payee, "Payment: Invalid payee address");
+        super.withdrawPayments(_payee);
+    }
 
-  function cancelPayments() public onlyOwner {
-    require(isApprove == false, "Payment: Payments already released");
-    super._asyncTransfer(msg.sender, address(this).balance);
-    super.withdrawPayments(payable(msg.sender));
-  }
+    function cancelPayments() public onlyOwner {
+        require(isApprove == false, "Payment: Payments already released");
+        super._asyncTransfer(msg.sender, address(this).balance);
+        super.withdrawPayments(payable(msg.sender));
+    }
 }
